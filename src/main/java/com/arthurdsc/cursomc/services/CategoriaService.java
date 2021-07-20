ce6db714dd15f804970188c6096c908d39a1b99a
ArthurCoimbra <arthurplank22@gmail.com>
2021-07-20 12:22:33 -0300

    Ajustes no endpoint /categorias/id com tratamento de exceção

--- src/main/java/com/arthurdsc/cursomc/services/CategoriaService.java
+++ src/main/java/com/arthurdsc/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.arthurdsc.cursomc.domain.Categoria;
 import com.arthurdsc.cursomc.repositories.CategoriaRepository;
+import com.arthurdsc.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -13,7 +14,11 @@ public class CategoriaService {
 	private CategoriaRepository categoriaRepository;
 	
 	public Categoria findCategoriaById (Long id) {
-		return categoriaRepository.findCategoriaById(id);
+		Categoria obj = categoriaRepository.findCategoriaById(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
+		}
+		return obj;
 	}
 
 }
