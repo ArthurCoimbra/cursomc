351b741c863ff00976b32d48a3aff8a61c192ec6
ArthurCoimbra <arthurplank22@gmail.com>
2021-08-13 12:13:51 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/arthurdsc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/arthurdsc/cursomc/resources/CategoriaResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,6 +46,7 @@ public class CategoriaResource {
 		return new ResponseEntity<>(categoria, HttpStatus.OK);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto){
 		Categoria obj = categoriaService.fromDTO(objDto);
@@ -52,7 +54,8 @@ public class CategoriaResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Void> update(@PathVariable Long id, @Valid @RequestBody CategoriaDTO objDto){
 		Categoria obj = categoriaService.fromDTO(objDto);
@@ -61,6 +64,7 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		categoriaService.delete(id);
